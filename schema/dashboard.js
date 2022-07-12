1abdd2dd8c83818c79c17e3d7d37e844a99318f7
saichandanaL <saichandana2021@gmail.com>
2022-07-12 19:23:36 +0530

    upadted

--- schema/dashboard.js
+++ schema/dashboard.js
@@ -1,34 +1,45 @@
 cube(`Dash`, {
-    sql: `select * from Dash`,
-    measures: {
-    //   count: {
-    //     type: `count`
-    //   },
-      rowCount: {
-        type: `count`,
-      },
-      quantityCount: {
-        type: `sum`,
-        sql: `Sales`
-      },
-      profit: {
-        type: `sum`,
-        sql: `Profit`
-      },
-      profitRatio: {
-        type:`sum`,
-        
-      }
+  sql: `select * from Dash`,
+  refreshKey:{
+     every:'1 minute',
+  },
+  measures: {
+  //   count: {
+  //     type: `count`
+  //   },
+    rowCount: {
+      type: `count`,
     },
-    dimensions: {
-      states: {
-        type: `string`,
-        sql: `State`
-      }
-    //   lang: {
-    //     type: `string`,
-    //     sql: `lang`
-    //   }
-    // }
-  }
-});
+    quantityCount: {
+      type: `sum`,
+      sql: `Sales`
+    },
+    profit: {
+      type: `runningTotal`,
+      sql: `Profit`,
+      format:`currency`
+    },
+    profitratio:{
+      sql: `${profit} / ${quantityCount}*100.0`,
+      type:`number`,
+      format:`percent`
+    }
+  },
+  dimensions: {
+    states: {
+      type: `string`,
+      sql: `State`,
+    },
+    SalesbyCategory:{
+      type:`string`,
+      sql:`Category`
+    },
+    Region:{
+      type:`string`,
+      sql:`Region`,
+      shown:false
+    }
+}
+}
+);
+
---
+++ views/data.js
@@ -0,0 +1,8 @@
+const Consumer = document.getElementById("con");
+
+Consumer.addEventListener("click",fun());
+
+function fun()
+{
+    alert('hello');
+}
