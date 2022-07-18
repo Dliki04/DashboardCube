c836fbf0b99ca730b804a8f33d69659ccba3fdd2
D.Likith Kumar <likithkumard@gmail.com>
2022-07-18 21:25:05 +0530

    ,,,,

--- schema/dashplus.js
+++ schema/dashplus.js
@@ -8,8 +8,27 @@ cube(`dashplus`, {
      sales:{
       sql:`Sales`,
       type:`sum`
-     }
-   
+     },
+     Central:{
+      sql:`Sales`,
+      type:`sum`,
+      filters: [{ sql: `${CUBE}.Region = 'Central'`}]
+    },
+    East:{
+      sql:`Sales`,
+      type:`sum`,
+      filters: [{ sql: `${CUBE}.Region = 'East'`}]
+    },
+    West:{
+      sql:`Sales`,
+      type:`sum`,
+      filters: [{ sql: `${CUBE}.Region = 'West'`}]
+    },
+    South:{
+      sql:`Sales`,
+      type:`sum`,
+      filters: [{ sql: `${CUBE}.Region = 'South'`}]
+    },
   }, 
   dimensions: { 
     shipdate:{
